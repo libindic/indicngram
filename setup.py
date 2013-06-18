ca1379f5ba28c8fcb23dd11294b282b53edebf9c
Diadara <nithin111@gmail.com>
2013-06-18 11:42:48 +0530

    pep8 cleaning

---
+++ setup.py
@@ -0,0 +1,22 @@
+
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+name = "ngram"
+
+setup(
+    name = name,
+    version = "0.2.1",
+    url = "http://silpa.org.in/NGram",
+    license = "LGPL-3.0",
+    description = "n-gram genereator for indic languages",
+    author = "Santhosh Thottingal",
+    author_email = "santhosh.thottingal@gmail.com",
+    long_description = """A n-gram generator for English, Hindi, Malayalam, Kannada and  Bengali""",
+    packages = find_packages(),
+    include_package_data = True,
+    setup_requires = ['setuptools-git'],
+    install_requires = ['setuptools','syllabalizer'],
+    zip_safe = False,
+    )
