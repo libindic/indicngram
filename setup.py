54856155c696cd943b5a74fbfbe3afeaef7ed178
Vasudev Kamath <kamathvasudev@gmail.com>
2013-08-17 13:31:24 +0530

    Version is 0.1 and wrap long description

--- setup.py
+++ setup.py
@@ -7,13 +7,14 @@ name = "ngram"
 
 setup(
     name = name,
-    version = "0.2.1",
+    version = "0.1",
     url = "http://silpa.org.in/NGram",
     license = "LGPL-3.0",
     description = "n-gram genereator for indic languages",
     author = "Santhosh Thottingal",
     author_email = "santhosh.thottingal@gmail.com",
-    long_description = """A n-gram generator for English, Hindi, Malayalam, Kannada and  Bengali""",
+    long_description = """A n-gram generator for English, Hindi,
+    Malayalam, Kannada and Bengali""",
     packages = find_packages(),
     include_package_data = True,
     setup_requires = ['setuptools-git'],
