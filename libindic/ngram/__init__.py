59ca8f394135d71f3d23fc343b29f50a1d1aa347
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2018-12-26 14:54:03 +0530

    Fix package imports

--- libindic/ngram/__init__.py
+++ libindic/ngram/__init__.py
@@ -18,8 +18,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #
-import indicsyllabifier
 
+from libindic.syllabifier import Syllabifier
 
 class Ngram:
     """
@@ -38,7 +38,7 @@ class Ngram:
         words = text.split(" ")
         ngrams = []
         for word in words:
-            s = indicsyllabifier.getInstance()
+            s = Syllabifier.getInstance()
             # TODO-Normalize before taking ngram!!!
             syllables = s.syllabify(word)
             syllable_count = len(syllables)
